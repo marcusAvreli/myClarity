6cc6d62be5847aaf42d97c659d127cc4549f0b5c
Mark Gavrilman <markgav19@gmail.com>
2024-05-08 18:21:22 +0300

    updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs'
 import fs from 'fs'
 import path from 'path'
-import rimraf from 'rimraf'
-
 
 //https://github.com/zambezi/ez-build/blob/d7f110b3cf136a3a6905d1c3723104a74ca1e5b8/src/builder/css.js#L17
 //https://github.com/niksy/css-loader/blob/0c8a23b48521656d8f2ea4c14108b44882ecb0f2/src/plugins/postcss-url-parser.js#L107
@@ -11,20 +9,12 @@ import rimraf from 'rimraf'
 //https://github.com/starzje/rollup_example/blob/62d146e10c941cbfeaec558f2f18efbd7ce958d5/rollup.config.mjs#L24
 //https://github.com/niklasgrewe/apple-svelte-demo/blob/9513ccda19e881010cbde46c6877445f387fdb48/rollup.config.js#L23
 import postcss from 'postcss';
-import postcssPlugin  from 'rollup-plugin-postcss'
-
 import packageJson from './package.json'
-
 import autoprefixer from 'autoprefixer';
 import resolve from 'rollup-plugin-node-resolve';
-
-import mkdirp from 'mkdirp';
 import {writeFileSync} from 'fs';
 import sass from 'rollup-plugin-sass'
-import cssnano from 'cssnano';
 import license from 'rollup-plugin-license';
-const buildFormats = ['cjs', 'es','umd'] // include others if needed
-
 const banner = ['/*!', packageJson.name, packageJson.version, '*/\n'].join(' ');
 
 
